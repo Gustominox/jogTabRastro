a5eb93d32bdc2ec15bc208a12bf860410e7a6d1c
Gustominox <agstcmps.111@gmail.com>
2020-03-14 15:21:40 +0000

    Correcoes indicadas pelos profs

--- rastro/dados.h
+++ rastro/dados.h
@@ -26,6 +26,6 @@ typedef struct {
 ESTADO *inicializar_estado();
 int obter_jogador_atual(ESTADO *estado);
 int obter_numero_de_jogadas(ESTADO *estado);
-CASA obter_estado_casa(ESTADO *e, COORDENADA c);
+CASA obter_estado_casa(ESTADO *e, int i, int j);
 
 #endif //RASTRO_DADOS_H
--- rastro/main.c
+++ rastro/main.c
@@ -7,15 +7,27 @@
 int main()
 {
     ESTADO *e = inicializar_estado();
-    /*
-        TESTES
     COORDENADA c;
     c.coluna = 6;
     c.linha = 6;
     jogar(e,c);
-    // VAZIO -> 0 | BRANCA -> 1 | PRETA -> 2
-    printf("%u\n%u\n%u",e->tab[3][3] ,e->tab[5][5] ,e->tab[4][4]);
-    */
+    /*
+        Representaçao   Posicoes na
+        Do Tabuleiro    Matriz
+        8|.......2      7|.......2
+        7|........      6|........
+        6|.....*..      5|.....*..
+        5|....#...      4|....#...
+        4|........      3|........
+        3|........      2|........
+        2|........      1|........
+        1|1.......      0|1.......
+          ABCDEFGH        01234567
+
+        VAZIO -> 0 | BRANCA -> 1 | PRETA -> 2
+     */
+    printf("%u\n%u\n%u\n",e->tab[3][4] ,e->tab[5][2] ,e->tab[4][3]);
+    mostrar_tabuleiro(e);
     interpretador(e);
     return 0;
 }

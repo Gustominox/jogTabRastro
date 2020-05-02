542fa2eae7e5b55dd37db5400c2f2c574173b375
Gustominox <agstcmps.111@gmail.com>
2020-05-02 18:01:10 +0100

    redes problemas

---
+++ rastro/bot.c
@@ -0,0 +1,5 @@
+//
+// Created by gusto on 5/2/20.
+//
+
+#include "bot.h"
---
+++ rastro/bot.h
@@ -0,0 +1,8 @@
+//
+// Created by gusto on 5/2/20.
+//
+
+#ifndef RASTRO_BOT_H
+#define RASTRO_BOT_H
+
+#endif //RASTRO_BOT_H
